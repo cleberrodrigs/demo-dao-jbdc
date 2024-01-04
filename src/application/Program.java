3b9eb3ab8425551b7f69135648236b4d8f21e73a
Cleber Rodrigues <clebermarkesrodrigues@gmail.com>
2024-01-04 18:43:22 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,5 +41,18 @@ public class Program {
 		
 		System.out.println("Inserted! New id = " + newSeller.getId());
 	
+		System.out.println("\n=== TEST 5: seller UpDate =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
+		
+		
+		
+		
+		
+		
+		
+		
 	} 
 }
